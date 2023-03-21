fb233bf438172e3d72510f616d0ae080a9175bf0
Santrosher <109319644+Santrosher@users.noreply.github.com>
2023-03-21 12:24:31 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,3 +5,8 @@ searchButton.addEventListener("click", () => {
   const query = searchInput.value;
   window.location.href = `https://www.google.com/search?q=${query}`;
 })
+
+document.getElementById("search-button").addEventListener("click", function() {
+        var searchQuery = document.getElementById("search-input").value;
+        window.location.href = "https://www.google.com/search?q=" + searchQuery;
+    });
