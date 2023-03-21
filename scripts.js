6f863cab6aec41348934575c83e2c06951fe9eec
Santrosher <sddvasquez2005@gmail.com>
2023-03-21 12:46:11 -0500

    updating html script

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 
-searchButton.addEventListener("click", () => {
-  const query = searchInput.value;
-  window.location.href = `https://www.google.com/search?q=${query}`;
-})
-
 document.getElementById("search-button").addEventListener("click", function() {
         var searchQuery = document.getElementById("search-input").value;
         window.location.href = "https://www.google.com/search?q=" + searchQuery;
-    });
+});
