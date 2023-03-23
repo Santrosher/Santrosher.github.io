80b83a021d31d149a1e75b540ea0b9bdc41e42bd
Santrosher <sddvasquez2005@gmail.com>
2023-03-23 10:13:52 -0500

    Adding scroll reveal

--- scripts.js
+++ scripts.js
@@ -5,3 +5,8 @@ document.getElementById("search-button").addEventListener("click", function() {
         var searchQuery = document.getElementById("search-input").value;
         window.location.href = "https://www.google.com/search?q=" + searchQuery;
 });
+
+ScrollReveal().reveal('.descarga');
+
+ScrollReveal().reveal('.descarga', 
+{ delay: 400, duration: 1000, distance: '50px', origin: 'left' });
